b7d13cb8de14c47d0afb995862d22458c6d82bfa
Trailmix05 <126606406+Trailmix05@users.noreply.github.com>
2023-12-10 16:54:23 +0100

    Push of website
    Push of all the files for the website

---
+++ Terminal/commands.js
@@ -0,0 +1,77 @@
+var linkedin = "https://www.linkedin.com/in/zachary-ivars/";
+var numtel = '+33 06 95 00 33 72'
+var pentester = 'https://www.cidj.com/metiers/pentester'
+
+whoami = [
+    "<br>",
+    "Prénom: Zachary",
+    "Nom: Ivars",
+    "Date de Naissance: 10 Mai 2005 (18 ans)",
+    "Occupation actuelle: Étudiant",
+    "Intérêts: Informatique, technologie, sciences, lecture, jeux vidéos, un peu de tout",
+    "<br>",
+    "Je suis une jeune personne qui s'intéresse à de divers milieu, <br>     me permettant d'avoir des sujets de discussions profondes et avec intérêts",
+    "Je suis parfaitement bilingue grâce à mon enfance aux États-Unis.", 
+    "Cette expérience a enrichi ma culture et mon adaptabilité à différentes situations.",
+    "Malgré mes performances dans le milieu scolaire, j'ai des difficultés avec le français(notamment à l'écrit).", 
+    "Même avec ces difficultés je suis sorti d'une filière générale (Maths, NSI, Maths expertes) avec mention bien.",
+    "Je suis actuellement en BUT informatique à l'IUT de Blagnac",
+  "<br>"
+];
+
+contact = [
+    "<br>",
+    'discord                     _trailmix',
+    'linkedin                    <a href="' + linkedin + '" target="_blank">linkedin/zacharyivars' + "</a>",
+    'numéro téléphone            ' + numtel,
+    'courrier éléctronique       <a href="mailto:zacharyivars@icloud.com">zacharyivars@icloud.com</a>"',
+    "<br>"
+];
+
+projets = [
+    "<br>",
+    "<strong>Objectif:</strong> Devenir Pentester/Hackeur Éthique",
+    '<a href="' + pentester + '" target="_blank">[* Plus d\'informations sur le métier]' + "</a>",
+    "",
+    "- Études:",
+    "==========",
+    '<strong>Choix 1:</strong>',
+    "- BUT Informatique",
+    "- École d'ingénieur (par préférence ENSEEIHT)",
+    "- Partir sur le marché du travail",
+    "<br>",
+    '<strong>Choix 2:</strong>',
+    "- MIT : Massachussets Institute of Technology",
+    "- Partir sur le marché du travail",
+    "<br>"
+];
+
+parcours = [
+    "<br>",
+    "2009-2018: <br>        Chataqua Lake Central School, Mayville, New York, United States of America",
+    "2018-2020: <br>        Collège: Notre Dames Des Victoires, Saint-Pourçain-sur-Sioule (03)",
+    "2020-2021: <br>        Lycée(2nde générale): Lycée Polyvalent Jean-Monnet, Yzeure (03)",
+    "2021-2023: <br>        Lycée(1ère et T générale): Lycée Générale Ferdinand Foch, Rodez (12)",
+    "2023-Actuellement: <br>        BUT: IUT de Blagnac (31)"
+]
+
+help = [
+    "<br>",
+    '<span class="command">whoami</span>         Qui suis-je?',
+    '<span class="command">projets</span>        Mes projets professionnels',
+    '<span class="command">parcours</span>       Mon parcours scolaire',
+    '<span class="command">contact</span>        Toutes les façons pour me contacter',
+    '<span class="command">history</span>        Affiche les dernières commandes utilisées',
+    '<span class="command">help</span>           Vous savez ce que cette commande fait',
+    '<span class="command">clear</span>          Efface le terminal',
+    "<br>",
+];
+
+banner = [
+    "",
+    '<span class="color2">Zachary Ivars | Étudiant à l\'IUT de Blagnac</span>',
+    '=======================================================================',
+    '<span class="color2">Bienvenu sur mon portfolio</span>',
+    "<span class=\"color2\">Pour une liste des commandes, taper: </span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+    "<span class=color2>Pour commencer à taper les commandes, cliquez après</class> visitors@zacharyivars.com:~$"
+];
